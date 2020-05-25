e17bd674e9ae173f4084676e1b1fb1bc7561da04
mlazzell17@gmail.com <MicheSi>
2020-05-25 13:52:45 -0700

    added buttons component

---
+++ csbw-game-of-life/src/components/buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Buttons = props => {
+    return (
+        <div className='buttonsDiv'>
+            <button>Start</button>
+
+        </div>
+    )
+}
+
+export default Buttons;
